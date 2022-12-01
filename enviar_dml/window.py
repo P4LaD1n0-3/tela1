7362931c4324e1450afb4f9fbf92a76f431a8e3f
Wesley Rolim <110367074+P4LaD1n0-3@users.noreply.github.com>
2022-12-01 18:50:01 -0300

    Add files via upload

--- enviar_dml/window.py
+++ enviar_dml/window.py
@@ -7,13 +7,13 @@ def btn_clicked():
 
 window = Tk()
 
-window.geometry("573x381")
+window.geometry("580x400")
 window.configure(bg = "#ffffff")
 canvas = Canvas(
     window,
     bg = "#ffffff",
-    height = 381,
-    width = 573,
+    height = 400,
+    width = 580,
     bd = 0,
     highlightthickness = 0,
     relief = "ridge")
@@ -28,119 +28,145 @@ background = canvas.create_image(
 ################################################################
 
 Change = Entry(
+    window,
     bd = 0,
-    bg = "#F4F8FA",
+    #bg = "#F4F8FA",
+    bg="blue",
     foreground="#696D6E",
     highlightthickness = 0,
     font = ("Roboto-Bold", int(10)))
 
 
 Change.place(
-    x = 100, y = 97,
+    x = 95, y = 60,
     width = 150,
     height = 22)
 
 Change.focus()
+
+
+################################################################
+
+Req = Entry(
+    window,
+    bd = 0,
+    #bg = "#F4F8FA",
+    bg="blue",
+    foreground="#696D6E",
+    highlightthickness = 0,
+    font = ("Roboto-Bold", int(10)))
+
+Req.place(
+    x = 95, y = 122,
+    width = 150,
+    height = 22)
+
 ################################################################
 
 Send_to = Entry(
+    window,
     bd = 0,
-    bg = "#F4F8FA",
+    #bg = "#F4F8FA",
+    bg="blue",
     foreground="#696D6E",
     highlightthickness = 0,
     font = ("Roboto-Bold", int(10)))
 
 Send_to.place(
-    x = 85, y = 166,
+    x = 85, y = 185,
     width = 180,
     height = 22)
 
 ################################################################
 
 Copia1 = Entry(
+    window,
     bd = 0,
-    bg = "#F4F8FA",
+    #bg = "#F4F8FA",
+    bg="blue",
     foreground="#696D6E",
     highlightthickness = 0,
     font = ("Roboto-Bold", int(10)))
 
 
 Copia1.place(
-    x = 85, y = 235,
+    x = 85, y = 244,
     width = 180,
     height = 22)
 
 ################################################################
 
 Copia2 = Entry(
+    window,
     bd = 0,
-    bg = "#F4F8FA",
+    #bg = "#F4F8FA",
+    bg="blue",
     foreground="#696D6E",
     highlightthickness = 0,
     font = ("Roboto-Bold", int(10)))
 
 
 Copia2.place(
-    x = 85, y = 312,
+    x = 85, y = 322,
     width = 180,
     height = 22)
 
 ################################################################
 Log = Text(
+    window,
     bd = 0,
-    bg = "#F4F8FA",
+    #bg = "#F4F8FA",
+    bg="blue",
     foreground="#696D6E",
     highlightthickness = 0,
     font = ("Roboto-Bold", int(10)))
 
 
 Log.place(
-    x = 304, y = 80,
+    x = 304, y = 58,
     width = 230,
-    height = 40)    
+    height = 86)    
     
 
 ################################################################
-EntryText = Entry(
+EntryText = Text(
+    window,
     bd = 0,
     #bg = "#F4F8FA",
     bg="blue",
     foreground="#696D6E",
     highlightthickness = 0,
-    justify='left',
     font = ("Roboto-Bold", int(10)))
 
 
 EntryText.place(
-    x = 304, y = 165,
+    x = 304, y = 182,
     width = 230,
-    height = 90,
-    
-    ) 
-
-
-
+    height = 86) 
 
 ################################################################
 
 File_Text = Text(
+    window,
     bd = 0,
-    bg = "#F4F8FA",
+    #bg = "#F4F8FA",
+    bg="blue",
     foreground="#696D6E",
     highlightthickness = 0,
     font = ("Roboto-Bold", int(10)))
 
 
 File_Text.place(
-    x = 335, y = 312,
+    x = 335, y = 322,
     width = 92,
-    height = 22)
+    height = 20)
 
 ################################################################
 
 
 Confirm = PhotoImage(file = f"Confirm.png")
 BtnConfirm = Button(
+    window,
     image = Confirm,
     borderwidth = 0,
     highlightthickness = 0,
@@ -148,7 +174,7 @@ BtnConfirm = Button(
     relief = "flat")
 
 BtnConfirm.place(
-    x = 477, y = 272,
+    x = 478, y = 281,
     width = 58,
     height = 20)
 
@@ -156,24 +182,26 @@ BtnConfirm.place(
 ################################################################
 
 
-Home = PhotoImage(file = f"Home.png")
-BtnHome = Button(
-    image = Home,
+Send = PhotoImage(file = f"Send.png")
+BtnSend = Button(
+    window,
+    image = Send,
     borderwidth = 0,
     highlightthickness = 0,
     command = btn_clicked,
     relief = "flat")
 
-BtnHome.place(
-    x = 480, y = 312,
-    width = 54,
-    height = 20)
+BtnSend.place(
+    x = 492, y = 321,
+    width = 35,
+    height = 17)
 
 
 ################################################################
 
 File = PhotoImage(file = f"File.png")
 BtnFile = Button(
+    window,
     image = File,
     borderwidth = 0,
     highlightthickness = 0,
@@ -181,7 +209,7 @@ BtnFile = Button(
     relief = "flat")
 
 BtnFile.place(
-    x = 428, y = 312,
+    x = 430, y = 320,
     width = 23,
     height = 20)
 
