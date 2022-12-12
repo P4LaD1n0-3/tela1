d30ac58c2f2aa5e9c506112c5ea0cc1aa9ac33e7
Wesley Rolim <110367074+P4LaD1n0-3@users.noreply.github.com>
2022-12-12 00:16:16 -0300

    Add files via upload

--- Dml_2/window.py
+++ Dml_2/window.py
@@ -24,5 +24,118 @@ background = canvas.create_image(
     425.0, 225.0,
     image=background_img)
 
+##########################################################################
+
+Change_ctask = Entry(
+    bd = 0,
+    bg="#F4F4F4",
+    highlightthickness = 0)
+
+Change_ctask.place(
+    x = 100.0, y = 77,
+    width = 180.0,
+    height = 26)
+
+##########################################################################
+
+Req_ritm = Entry(
+    bd = 0,
+    bg="#F4F4F4",
+    highlightthickness = 0)
+
+Req_ritm.place(
+    x = 100.0, y = 154,
+    width = 180.0,
+    height = 26)
+
+##########################################################################
+
+send_to = Entry(
+    bd = 0,
+    bg="#F4F4F4",
+    highlightthickness = 0)
+
+send_to.place(
+    x = 90.0, y = 229,
+    width = 194.0,
+    height = 26)
+
+##########################################################################
+
+copia1 = Entry(
+    bd = 0,
+    bg="#F4F4F4",
+    highlightthickness = 0)
+
+copia1.place(
+    x = 90.0, y = 304,
+    width = 194.0,
+    height = 26)
+
+##########################################################################
+
+copia2 = Entry(
+    bd = 0,
+    bg="#F4F4F4",
+    highlightthickness = 0)
+
+copia2.place(
+    x = 90.0, y = 379,
+    width = 194.0,
+    height = 26)
+
+
+##########################################################################
+
+file = Entry(
+    bd = 0,
+    bg="#F4F4F4",
+    highlightthickness = 0)
+
+file.place(
+    x = 362.0, y = 379,
+    width = 170.0,
+    height = 26)
+
+
+##########################################################################
+
+Log = Entry(
+    bd = 0,
+    bg="#F4F4F4",
+    highlightthickness = 0)
+
+Log.place(
+    x = 320.0, y = 78,
+    width = 495.0,
+    height = 101)
+
+##########################################################################
+
+EntryLog = Entry(
+    bd = 0,
+    bg="#F4F4F4",
+    highlightthickness = 0)
+
+EntryLog.place(
+    x = 320.0, y = 230,
+    width = 495.0,
+    height = 101)
+
+##########################################################################
+
+File = PhotoImage(file = f"File.png")
+FileBtn = Button(
+    image = File,
+    borderwidth = 0,
+    highlightthickness = 0,
+    command = btn_clicked,
+    relief = "flat")
+
+FileBtn.place(
+    x = 535, y = 380,
+    width = 27,
+    height = 25)
+
 window.resizable(False, False)
 window.mainloop()
